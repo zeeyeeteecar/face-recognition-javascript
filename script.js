782cd0652fa6580d1fd7c3103a019b8246335f93
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2023-09-13 11:15:13 -0700

    2

--- script.js
+++ script.js
@@ -57,11 +57,16 @@ video.addEventListener("play", async () => {
     const detections = await faceapi
       .detectAllFaces(video)
       .withFaceLandmarks()
+      .withFaceExpressions()
       .withFaceDescriptors();
 
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
 
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+    
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
     const results = resizedDetections.map((d) => {
       return faceMatcher.findBestMatch(d.descriptor);
